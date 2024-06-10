8ea30db300f956cddae44c14352a877be4c0c01f
Steve <148709785+Steveqks@users.noreply.github.com>
2024-06-11 00:35:37 +0800

    Created homepage for super & company admin
    homepage with links to working features..
    
    login method not implemented

---
+++ companyadmin_homepage.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="a.css">
+
+    <title>TrackMySchedule</title>
+</head>
+<body>
+
+    <!-- Top Section -->
+	<div style="border: 1px solid black; height: 20vh; overflow: hidden; text-align: left;">
+        <img src="tms.png" alt="TrackMySchedule Logo" style="height: 100%; width: auto;">
+    </div>
+
+    <!-- Middle Section -->
+    <div style="display: flex; border: 1px solid black; height: 80vh;">
+        
+        <!-- Left Section (Navigation) -->
+			<div class="vertical-menu" style="border-right: 1px solid black; padding: 0px;">
+			  <a href="#">Home</a>
+			  <a href="#">Link 1</a>
+			  <a href="#">Link 2</a>
+			  <a href="#">Link 3</a>
+			  <a href="#">Link 4</a>
+			</div>
+        
+        <!-- Right Section (Activity) -->
+        <div style="width: 80%; padding: 10px;">
+
+            <!-- Add more content as needed -->
+			&lt;&lt;homepage&gt;&gt;
+			<br>
+			1.  <a href="companyadmin_create_specialisation.php">Create Specialisation</a>
+			<br>
+			
+			2.  <a href="companyadmin_viewdelete_specialisation.php">View/Edit/Delete Specialisation</a>
+			<br>
+			
+			3...
+			<br>
+			
+			4...
+			
+			
+        </div>
+    </div>
+
+</body>
+</html>
+
+
