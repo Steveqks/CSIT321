ba679cf4b4ba020a96b616d2ae71990533fc3cc9
raymar1234 <jrhealerz@gmail.com>
2024-06-21 14:19:08 +0800

    removed 'specialisation' field
    removed 'specialisation' field

--- update_account.php
+++ update_account.php
@@ -20,7 +20,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['password']);
     $confirm_password = htmlspecialchars($_POST['confirm_password']);
-    $specialisation = htmlspecialchars($_POST['specialisation']);
 
     // Check if passwords match
     if ($password != $confirm_password) {
@@ -29,9 +28,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Prepare and execute the update statement
-    $sql = "UPDATE existinguser SET FirstName = ?, LastName = ?, Email = ?, Password = ?, SpecialisationID = ? WHERE UserID = 1";
+    $sql = "UPDATE existinguser SET FirstName = ?, LastName = ?, Email = ?, Password = ? WHERE UserID = 1";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssssi", $first_name, $last_name, $email, $password, $specialisation);
+    $stmt->bind_param("ssss", $first_name, $last_name, $email, $password);
     
     if ($stmt->execute()) {
         header("Location: acc_details_pt.php?message=Account details updated successfully.");
