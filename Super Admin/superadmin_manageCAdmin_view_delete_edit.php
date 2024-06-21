f11260076fa2fe3ff6f1389c9e362e0ee13704f7
Steve <148709785+Steveqks@users.noreply.github.com>
2024-06-21 15:50:39 +0800

    for super admin, added links to navigation tab

--- Super Admin/superadmin_manageCAdmin_view_delete_edit.php
+++ Super Admin/superadmin_manageCAdmin_view_delete_edit.php
@@ -26,11 +26,13 @@ session_start();
         
         <!-- Left Section (Navigation) -->
 			<div class="vertical-menu" style="border-right: 1px solid black; padding: 0px;">
-			  <a href="#">Home</a>
-			  <a href="#">Link 1</a>
-			  <a href="#">Link 2</a>
-			  <a href="#">Link 3</a>
-			  <a href="#">Link 4</a>
+			  <a href="superadmin_homepage.php">Home</a>
+			  <a href="superadmin_ManageAccount.php">Manage Account</a>
+			  <a href="superadmin_manageCompany_create.php">Manage Company > Create Company </a>
+			  <a href="superadmin_manageCompany_view.php">Manage Company > View Company </a>
+			  <a href="superadmin_manageCAdmin_approve_unreg_user.php">Approve New Company (Create New Company & Company Admin)</a>
+			  <a href="superadmin_manageCAdmin_create.php">Manage Company Admin > Create Company Admin</a>
+			  <a href="superadmin_manageCAdmin_view_delete.php">Manage Company Admin > View Company Admin</a>
 			</div>
         
         <!-- Right Section (Activity) -->
