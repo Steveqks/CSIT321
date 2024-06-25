1af88353059b9d4a9fe7200672771a0b79479ee7
Shane-Kyle Chua <90674209+Kairu1999@users.noreply.github.com>
2024-06-25 15:07:17 +0800

    Fixed Code Structure for Unregistered Users
    Fixed code structure for unregistered users enter credentials function

--- Unregistered Users/HomePage.php
+++ Unregistered Users/HomePage.php
@@ -30,49 +30,40 @@
 			
 			$verify_query = mysqli_query($db, "SELECT email FROM unregisteredusers where Email = '$email'");
 			
-			//Regex validation for UEN:
-			if(strlen($companyuen) == 9)
+			
+			if(mysqli_num_rows($verify_query) != 0)
 			{
-				//Check for 9 character UEN
-				if(mysqli_num_rows($verify_query) != 0)
-				{
-					echo "<div class='message'>
-						  <p>This email is used, Try another One Please!</p>
-					  </div> <br>";
-				}
-				else
-				{
-				mysqli_query($db,"INSERT INTO unregisteredusers(Email,Password,UEN,CompanyName,FirstName,LastName,PlanID) VALUES('$email','$password','$companyuen','$companyname','$firstname','$lastname','$planschoice')") or die("Error Occured");
-
-					echo "<div class='message'>
-						  <p>Credentials entered successfully!</p>
-					  </div> <br>";
-				}
-			}
-			else if(strlen($companyuen) == 10)
-			{
-				//Check for 10 character UEN
-				//Check for 9 character UEN
-				if(mysqli_num_rows($verify_query) != 0)
-				{
-					echo "<div class='message'>
-						  <p>This email is used, Try another One Please!</p>
-					  </div> <br>";
-				}
-				else
-				{
-				mysqli_query($db,"INSERT INTO unregisteredusers(Email,Password,UEN,CompanyName,FirstName,LastName,PlanID) VALUES('$email','$password','$companyuen','$companyname','$firstname','$lastname','$planschoice')") or die("Error Occured");
-
-					echo "<div class='message'>
-						  <p>Credentials entered successfully!</p>
-					  </div> <br>";
-				}
+				echo "<div class='message'>
+					  <p>This email is used, Try another One Please!</p>
+				  </div> <br>";
 			}
 			else
 			{
-				echo "<div class='message'>
-						  <p>Invalid UEN Length, Please enter a UEN which is 9 or 10 characters!</p>
+				//Check for 9 character UEN
+				if(strlen($companyuen) == 9)
+				{
+					mysqli_query($db,"INSERT INTO unregisteredusers(Email,Password,CompanyName,CompanyUEN,FirstName,LastName,PlanID) VALUES('$email','$password','$companyname','$companyuen','$firstname','$lastname','$planschoice')") or die("Error Occured");
+
+					echo "<div class='message'>
+						  <p>Credentials entered successfully!</p>
 					  </div> <br>";
+				}
+				//Check for 10 character UEN
+				else if(strlen($companyuen) == 10)
+				{
+					mysqli_query($db,"INSERT INTO unregisteredusers(Email,Password,CompanyName,CompanyUEN,FirstName,LastName,PlanID) VALUES('$email','$password','$companyname','$companyuen','$firstname','$lastname','$planschoice')") or die("Error Occured");
+
+					echo "<div class='message'>
+						  <p>Credentials entered successfully!</p>
+					  </div> <br>";
+				}
+				//Throw Error if UEN Doesnt match
+				else
+				{
+					echo "<div class='message'>
+							  <p>Invalid UEN Length, Please enter a UEN which is 9 or 10 characters!</p>
+						  </div> <br>";
+				}
 			}
 			
 		}
@@ -96,13 +87,13 @@
                <div id = "RegisterForm">
                     <h2>Empower Your Team, Simplify Your Schedule</h2>
 					<form action = "" method = "post">
-						<input id = "firstname" name = "firstname" type = "text" placeholder = "First Name">
-						<input id = "lastname" name = "lastname" type = "text" placeholder = "Last Name">
-						<input id = "email" name = "email" type = "text" placeholder = "Email Address">
-						<input id = "password" name = "password" type = "password" placeholder = "Password">
-						<input id = "companyuen" name = "companyuen" type = "text" placeholder = "Company UEN(Unique Entity Number)">
-						<input id = "companyname" name = "companyname" type = "text" placeholder = "Company Name">
-						<select id = "planschoice" name = "plans">
+						<input id = "firstname" name = "firstname" type = "text" placeholder = "First Name" required>
+						<input id = "lastname" name = "lastname" type = "text" placeholder = "Last Name" required>
+						<input id = "email" name = "email" type = "text" placeholder = "Email Address" required>
+						<input id = "password" name = "password" type = "password" placeholder = "Password" required>
+						<input id = "companyname" name = "companyname" type = "text" placeholder = "Company Name" required>
+						<input id = "companyuen" name = "companyuen" type = "text" placeholder = "Company UEN(Unique Entity Number)" required>
+						<select id = "planschoice" name = "plans" required>
 							<option value = "1">Tier 1($9.99/month)</option>
 							<option value = "2">Tier 2($29.99/month)</option>
 							<option value = "3">Tier 3($59.99/month)</option>
