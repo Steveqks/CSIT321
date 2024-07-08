72d59811b444588689e7cf4297b62780e7f11800
Steve <148709785+Steveqks@users.noreply.github.com>
2024-07-08 21:28:55 +0800

    added superadmin>change password feature

--- Super Admin/superadmin_ManageAccount.php
+++ Super Admin/superadmin_ManageAccount.php
@@ -1,7 +1,94 @@
 <?php
 session_start();
+				
+	$SAdminID = $_SESSION['SAdminID'];
+	
+	if(isset($_POST['newEmail'])){
+		$newFirstName = $_POST['newFirstName'];			
+		$newLastName = $_POST['newLastName'];			
+		$newEmail = $_POST['newEmail'];			
+		$newPassword = $_POST['newPassword'];			
+		
+		//check if there are changes in first name
+		if ($_POST['oldFirstName'] != $_POST['newFirstName']){
+			$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
+				$result2 = mysqli_query($db,"UPDATE superadmin SET FirstName = '$newFirstName' WHERE SAdminID = '$SAdminID' ") or die("update Error");
+				$_SESSION['message1'] = "<p>First name has been changed.</p>";
+			
+		}
+		else $_SESSION['message1'] = "";
+		
+		//check if there are changes in last name
+		if(@$_POST['oldLastName'] != $newLastName){
+			$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
+			$result2 = mysqli_query($db,"UPDATE superadmin SET LastName = '$newLastName' WHERE SAdminID = '$SAdminID'") or die("update Error");
+			$_SESSION['message2'] = "<p>Last name has been changed.</p>";
+		}
+		else $_SESSION['message2'] = "";
+		
+		//if email is changed
+		if(@$_POST['oldEmail'] != $newEmail){
+			
+			$sql = "SELECT * FROM companyadmin WHERE Email = '$newEmail'";
+			$qres = mysqli_query($db, $sql); 
+			$num_rows=mysqli_num_rows($qres);
 
-
+			// exists
+			if($num_rows > 0){
+				//return error
+				$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
+			}
+			// dont exists
+			else{
+				$sql = "SELECT * FROM superadmin WHERE Email = '$newEmail'";
+				$qres = mysqli_query($db, $sql); 
+				$num_rows=mysqli_num_rows($qres);
+				
+				// exists
+				if($num_rows > 0){
+					//return error
+					$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
+				}
+				// dont exists
+				else{
+					// exists
+					if($num_rows > 0){
+						//return error
+						$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
+					}
+					// dont exists
+					else{
+						$sql = "SELECT * FROM existinguser WHERE Email = '$newEmail'";
+						$qres = mysqli_query($db, $sql); 
+						$num_rows=mysqli_num_rows($qres);
+					
+						if($num_rows > 0){
+						//return error
+						$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
+						}
+						// dont exists
+						else{
+							$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
+							$result2 = mysqli_query($db,"UPDATE superadmin SET Email = '$newEmail' WHERE SAdminID = '$SAdminID'") or die("update Error");
+							$_SESSION['message3'] = "<p>Email Address has been changed.</p>";
+						}
+					}
+				}
+			}
+		}
+		else $_SESSION['message3'] = "";
+		
+		//check if there are changes in password
+		if(@$_POST['oldPassword'] != $newPassword){
+			$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
+			$result2 = mysqli_query($db,"UPDATE superadmin SET Password = '$newPassword' WHERE SAdminID = '$SAdminID'") or die("update Error");
+			$_SESSION['message4'] = "<p>Password has been changed.</p>";
+		}
+		else $_SESSION['message4'] = "";
+		
+		header('Location: superadmin_ManageAccount.php');
+		exit;
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,8 +117,7 @@ session_start();
 		
 			<h2>Manage Account</h2>
 			<?php   
-				$SAdminID = $_SESSION['SAdminID'];
-				$confirmMSG = 'want to confirm?';	
+
 				
 				//get Super Admin data
 				$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
@@ -49,6 +135,7 @@ session_start();
 										First Name: <input type='text' name='oldFirstName' value='" . $Row['FirstName'] . "' readonly><br>
 										Last Name: <input type='text' name='oldLastName' value='" . $Row['LastName'] . "' readonly> <br>
 										Email Address: <input type='text' name='oldEmail' value=" . $Row['Email'] . " readonly> <br>
+										Password: <input type='password' name='oldPassword' value=" . $Row['Password'] . " readonly> <br>
 									<br></td>
 											
 									<td style='border: 2px solid black; border-collapse: collapse;'> 
@@ -57,7 +144,7 @@ session_start();
 										First Name: <input type='text' name='newFirstName' value='" . $Row['FirstName'] . "' maxlength='16'> <br>
 										Last Name: <input type='text' name='newLastName' value='" . $Row['LastName'] . "' maxlength='16'><br>
 										Email Address: <input type='text' name='newEmail' value=" . $Row['Email'] . " maxlength='32'><br>
-										
+										Password: <input type='password' name='newPassword' value=" . $Row['Password'] . " > <br>
 										<input type='button' value='Update' onclick='confirmDiag();'>
 									</td>
 								</tr>
@@ -70,84 +157,9 @@ session_start();
 				echo @$_SESSION['message1'];
 				echo @$_SESSION['message2'];
 				echo @$_SESSION['message3'];
+				echo @$_SESSION['message4'];
 				
-				if(isset($_POST['newEmail'])){
-					$newFirstName = $_POST['newFirstName'];			
-					$newLastName = $_POST['newLastName'];			
-					$newEmail = $_POST['newEmail'];			
-					
-					//check if there are changes in first name
-					if ($_POST['oldFirstName'] != $_POST['newFirstName']){
-						$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
-							$result2 = mysqli_query($db,"UPDATE superadmin SET FirstName = '$newFirstName' WHERE SAdminID = '$SAdminID' ") or die("update Error");
-							$_SESSION['message1'] = "<p>First name has been changed.</p>";
-						
-					}
-					else $_SESSION['message1'] = "";
-					
-					//check if there are changes in last name
-					if(@$_POST['oldLastName'] != $newLastName){
-						$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
-						$result2 = mysqli_query($db,"UPDATE superadmin SET LastName = '$newLastName' WHERE SAdminID = '$SAdminID'") or die("update Error");
-						$_SESSION['message2'] = "<p>Last name has been changed.</p>";
-					}
-					else $_SESSION['message2'] = "";
-					
-					//if email is changed
-					if(@$_POST['oldEmail'] != $newEmail){
-						
-						$sql = "SELECT * FROM companyadmin WHERE Email = '$newEmail'";
-						$qres = mysqli_query($db, $sql); 
-						$num_rows=mysqli_num_rows($qres);
-
-						// exists
-						if($num_rows > 0){
-							//return error
-							$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
-						}
-						// dont exists
-						else{
-							$sql = "SELECT * FROM superadmin WHERE Email = '$newEmail'";
-							$qres = mysqli_query($db, $sql); 
-							$num_rows=mysqli_num_rows($qres);
-							
-							// exists
-							if($num_rows > 0){
-								//return error
-								$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
-							}
-							// dont exists
-							else{
-								// exists
-								if($num_rows > 0){
-									//return error
-									$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
-								}
-								// dont exists
-								else{
-									$sql = "SELECT * FROM existinguser WHERE Email = '$newEmail'";
-									$qres = mysqli_query($db, $sql); 
-									$num_rows=mysqli_num_rows($qres);
-								
-									if($num_rows > 0){
-									//return error
-									$_SESSION['message3'] = "<p>Email Address is already in use.</p>";
-									}
-									// dont exists
-									else{
-										$db = mysqli_connect('localhost','root','','tms') or die("Couldnt Connect to database");
-										$result2 = mysqli_query($db,"UPDATE superadmin SET Email = '$newEmail' WHERE SAdminID = '$SAdminID'") or die("update Error");
-										$_SESSION['message3'] = "<p>Email Address has been changed.</p>";
-									}
-								}
-							}
-						}
-					}
-					else $_SESSION['message3'] = "";
-					
-					header('Location: superadmin_ManageAccount.php');
-					exit;
-				}
+				
 			?>
         </div>
     </div>
